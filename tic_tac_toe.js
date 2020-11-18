4192abb638acb1d7299578a5effe13ce920a2bb5
Ezio112 <44439644+Ezio112@users.noreply.github.com>
2020-11-18 13:09:31 +0530

    Update tic_tac_toe.js

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,30 +1,67 @@
-var table=document.querySelector(".gameBoard");
+var table=document.querySelector(".gameBoard"), turnShow=document.querySelector("#turn");
+var scorePlayer1=0,scorePlayer2=0;
+var currentPlayer=1;
+var mark='X',otherMark='O';
 
 function getBlock(i,j){
   return table.querySelector("."+"i"+i+"j"+j);
 }
-function drawX(i,j){
-  var block=getBlock(i,j);
-  block.addEventListener("click",function(){
-    block.textContent="X";
-  });
+
+function markAt(i,j){
+  return getBlock(i,j).textContent;
 }
-function drawO(i,j){
-  var block=getBlock(i,j);
-  block.addEventListener('dblclick',function(){
-    block.textContent="O";
-  });
-}
-for(var i=0;i<3;++i){
-  for(var j=0;j<3;++j){
-    drawX(i,j);
-    drawO(i,j);
+function checkWinner(){
+  for(var i=0;i<3;++i){
+    if(markAt(i,0)==markAt(i,1) && markAt(i,0)==markAt(i,2) && (markAt(i,0)=="X" || markAt(i,0)=="O")){
+      return true;
+    }
+    if(markAt(0,i)==markAt(1,i) && markAt(0,i)==markAt(2,i) && (markAt(0,i)=="X" || markAt(0,i)=="O")){
+      return true;
+    }
   }
+  if(markAt(0,0)==markAt(1,1) && markAt(0,0)==markAt(2,2) && (markAt(0,0)=="X" || markAt(0,0)=="O")){       ///Case of principal diagonal
+    return true;
+  }
+  if(markAt(2,0)==markAt(1,1) && markAt(2,0)==markAt(0,2) && (markAt(1,1)=="X" || markAt(1,1)=="O")){     ///Case of off diagonal.
+    return true;
+  }
+  return false;   //default case;
 }
-document.querySelector("#restart").addEventListener('click',function(){
+function resetButton(){
   for(var i=0;i<3;++i){
     for(var j=0;j<3;++j){
       getBlock(i,j).textContent="";
     }
   }
-})
+}
+
+function draw(i,j){
+  var block=getBlock(i,j);
+  block.addEventListener("click",function(){
+    block.textContent=mark;
+    if(checkWinner()){
+      alert("Player "+currentPlayer+" Won the game!.\nReseting the board.");
+      if(currentPlayer==1){
+        ++scorePlayer1;
+        document.querySelector("#scorePlayer1").textContent="Player 1: "+scorePlayer1;
+      }
+      else{
+        ++scorePlayer2;
+        document.querySelector("#scorePlayer2").textContent="Player 2: "+scorePlayer2;
+      }
+      resetButton();
+    }
+    else{
+      [mark,otherMark]=[otherMark,mark];        //Changing the to put board sign
+      currentPlayer=3-currentPlayer;            //Changing the current player
+      turnShow.textContent="Player "+currentPlayer+"\'s turn."
+    }
+  });
+}
+for(var i=0;i<3;++i){
+  for(var j=0;j<3;++j){
+    draw(i,j);
+  }
+}
+
+document.querySelector("#restart").addEventListener('click',resetButton);
